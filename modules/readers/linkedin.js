0826aa35578f0f3c5f11158fe4cba02dd99cc22c
Julio <juliocnl92@gmail.com>
2026-06-24 11:31:09 -0700

    add constant type to readers

--- modules/readers/linkedin.js
+++ modules/readers/linkedin.js
@@ -1,5 +1,7 @@
 import { newJob } from '../state.js';
 
+const TYPE = 'linkedin'
+
 function bodyReader(){
     const section = document.querySelector('section[aria-label="Contenido principal"]');
     const root = section?.firstElementChild ?.firstElementChild?.firstElementChild;
@@ -11,13 +13,8 @@ function bodyReader(){
 function urlReader(url) {
     const regex = /https:\/\/www\.linkedin\.com\/jobs\/view\/\d+/;
     const match = url.match(regex);
-    if (match) return newJob(match[0], 'linkedin')
+    if (match) return newJob(match[0], TYPE)
     return undefined
 }
 
-export const linkedinReader =
-    {
-        type: "linkedin",
-        body: bodyReader, 
-        url: urlReader
-    }
+export const linkedinReader = {type: TYPE, body: bodyReader, url: urlReader}
